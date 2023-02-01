b17797fde7444b5e538c33897a16d73d2e1b1407
elcrity <elcrity@naver.com>
2023-02-01 17:48:49 +0900

    230201 - Add a button to an app, lemonade

--- 021DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ 021DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
     class MainActivity : AppCompatActivity() {
@@ -15,14 +15,44 @@ import androidx.appcompat.app.AppCompatActivity
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        rollDice()
     }
 
+
     private fun rollDice() {
+        //6면체 주사위 생성
         val dice = Dice(6)
-        val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextView2 : TextView = findViewById(R.id.textView2)
-        resultTextView.text = dice.roll().toString()
-        resultTextView2.text = dice.roll().toString()
+        //1~6까지 랜덤 숫자
+        val diceRoll = dice.roll()
+        val diceRoll2 = dice.roll()
+        //diceImage id값
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        //나온 숫자에 맞는 이미지 id 입력
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        //imageView를 입력받은 이미지 id로 설정
+        diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
+        //내용 설명 업데이트
+        diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll.toString()
     }
 }
 
