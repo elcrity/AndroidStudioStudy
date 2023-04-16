493f46087366f28a78494a709371d73967736a4f
elcrity <elcrity@naver.com>
2023-04-16 16:28:51 +0900

    230416 이미지표시

--- 0511GetDataFromInternet/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ 0511GetDataFromInternet/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -1,11 +1,14 @@
 package com.example.android.marsphotos
 
+import android.media.Image
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.MarsApiStatus
 import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl")//뷰 항목에 imageUrl속성이 있는 경우 이 결합 어댑터 실행
@@ -26,5 +29,24 @@ fun bindRecyclerView(recyclerView: RecyclerView,
                      data: List<MarsPhoto>?) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+
+}
+
+@BindingAdapter("marsApiStatus")//맞춤 속성 marsApiStatus를 매개변수로 전달하는 @BindingAdapter 주석을 메서드에 추가합니다.
+fun bindStatus(statusImageView : ImageView, status : MarsApiStatus? ){
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+        else -> {}
+    }
 
 }
